d54d870a5e307c9997516b0ead38ffd058c24265
shweta <bawaneshweta1994@gmail.com>
2023-05-24 12:24:35 +0530

    vpc1

--- firstScript/main.tf
+++ firstScript/main.tf
@@ -30,10 +30,10 @@ resource "aws_internet_gateway" "igw" {
 }
 
 resource "aws_default_route_table" "default-rt" {
-    default_route_table_id = aws_default_route_table.id
+    default_route_table_id = aws_vpc.my-vpc1.default_route_table_id  
     route = {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw.id
+        gateway_id = aws_internet_gateway.igw
     }
   
 }
